a989bf681817877422b593e24bcf6c72971e3372
Forgemaster <forgemaster@superinstance.dev>
2026-05-08 17:13:38 -0800

    chore: add .gitignore, remove build artifacts

--- target/debug/deps/integration-88436c830606be4b.d
+++
@@ -1,5 +0,0 @@
-/tmp/fleet-proto-rs/target/debug/deps/integration-88436c830606be4b.d: tests/integration.rs
-
-/tmp/fleet-proto-rs/target/debug/deps/integration-88436c830606be4b: tests/integration.rs
-
-tests/integration.rs:
